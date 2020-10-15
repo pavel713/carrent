ccb8199208a12bf2f21a66e7cc094712f3bfc52c
Pavel Strezhnev <pavelstrezhnev@gmail.com>
2020-10-16 01:37:50 +0300

    Logout fixed

--- dao/src/main/java/com/carrent/dao/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carrent.dao.repository;
-
-import com.carrent.dao.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>  {
-}
